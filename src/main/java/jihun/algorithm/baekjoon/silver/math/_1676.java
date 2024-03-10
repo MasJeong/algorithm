75152346e932800d68997cfa116cb9fb0b0a1f98
MasJeong <jihun3654@naver.com>
2024-03-10 19:48:09 +0900

    팩토리얼 0의 개수 - 수학 (조합)

---
+++ src/main/java/jihun/algorithm/baekjoon/silver/math/_1676.java
@@ -0,0 +1,38 @@
+package jihun.algorithm.baekjoon.silver.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+/**
+ * 팩토리얼 0의 개수 - 수학 (조합)
+ */
+public class _1676 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        BigInteger result = BigInteger.ONE;
+
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        String s = String.valueOf(result);
+
+        int cnt = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                cnt++;
+            }  else {
+                break;
+            }
+        }
+
+        System.out.println(cnt);
+
+        br.close();
+    }
+
+}
