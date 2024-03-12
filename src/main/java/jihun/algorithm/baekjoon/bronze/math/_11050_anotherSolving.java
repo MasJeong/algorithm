ee6aea80199e4e0a753aa52120f1a19dba86ab50
jeongjihun <jihun3654@naver.com>
2024-03-12 13:25:21 +0900

    이항 계수1 - 조합론 다른 방법 풀이 작업중

---
+++ src/main/java/jihun/algorithm/baekjoon/bronze/math/_11050_anotherSolving.java
@@ -0,0 +1,34 @@
+package jihun.algorithm.baekjoon.bronze.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 이항 계수1 - 수학(조합론)
+ */
+public class _11050_anotherSolving {
+
+    private static int fibonacci(int num) {
+        if (num == 0) return 1;
+
+        return num * fibonacci(num - 1);
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        // TODO 이항계수, 파스칼 법칙을 사용하여 다시 풀어보자
+
+        // nCr = n! / (n - r)! * r!
+        System.out.println(fibonacci(n) / (fibonacci(n - k) * fibonacci(k)));
+        br.close();
+    }
+
+}
