c1bc2e9c665fca9e76c0ad3d5e75e0f76a8a611a
jeongjihun <jihun3654@naver.com>
2024-03-12 13:25:06 +0900

    불필요 객체 제거

--- src/main/java/jihun/algorithm/baekjoon/bronze/math/_11050.java
+++ src/main/java/jihun/algorithm/baekjoon/bronze/math/_11050.java
@@ -13,7 +13,6 @@ public class _11050 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
