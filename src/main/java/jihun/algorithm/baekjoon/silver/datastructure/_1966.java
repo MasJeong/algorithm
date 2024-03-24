524e7b7477957c2c81a42518940f3730c0bdb2fb
MasJeong <jihun3654@naver.com>
2024-03-24 16:47:21 +0900

    프린터 큐 작업중

--- src/main/java/jihun/algorithm/baekjoon/silver/datastructure/_1966.java
+++ src/main/java/jihun/algorithm/baekjoon/silver/datastructure/_1966.java
@@ -7,38 +7,72 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 /**
- * 프린터 큐 - TODO 작업중
+ * 프린터 큐 - 우선순위 큐
  */
 public class _1966 {
 
     @AllArgsConstructor
     static class Printer {
-        int a;
+        int pointer;
+        int priority;
     }
 
     // 클래스 객체의 우선순위를 위한 클래스
     static class PrinterComparator implements Comparator<Printer> {
         @Override
         public int compare(Printer o1, Printer o2) {
-            if (o1.a == o2.a) {
-                return o2.a - o1.a;
-            } else {
-                return o1.a - o2.a;
+            if (o1.priority != o2.priority) {
+                return o2.priority - o1.priority;
             }
+
+            return 0;
         }
     }
 
     public static void main(String[] args) throws IOException {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
 
         // 클래스 객체에 대한 우선순위 기준 제공
-        final PriorityQueue<Printer> priorityQueue = new PriorityQueue<>(100, new PrinterComparator());
+        final PriorityQueue<Printer> pq = new PriorityQueue<>(100, new PrinterComparator());
 
-        int n = Integer.parseInt(br.readLine());
-        
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
-        priorityQueue.offer(new Printer(1));
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < n; i++) {
+                int priority = Integer.parseInt(st.nextToken());
+
+                if (m == i) {
+                    pq.offer(new Printer(m, priority));
+                } else {
+                    pq.offer(new Printer(0, priority));
+                }
+            }
+
+            for (int i = 1; i <= n; i++) {
+                Printer printer = pq.poll();
+
+                // TODO 작업중
+                if (printer.pointer == m) {
+                    sb.append(i).append("\n");
+                    break;
+                }
+            }
+
+            pq.clear();
+        }
+
+        System.out.println(sb);
+        br.close();
     }
 }
