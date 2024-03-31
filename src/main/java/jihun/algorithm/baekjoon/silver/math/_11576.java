33abc593e573f662dd013a9f37c53ffb21a999fd
MasJeong <jihun3654@naver.com>
2024-03-31 20:10:46 +0900

    Base Conversion 추가

--- src/main/java/jihun/algorithm/baekjoon/silver/math/_11576.java
+++ src/main/java/jihun/algorithm/baekjoon/silver/math/_11576.java
@@ -3,19 +3,46 @@ package jihun.algorithm.baekjoon.silver.math;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 /**
- * Base Conversion - 수학 TODO
+ * Base Conversion - 수학
  */
 public class _11576 {
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
 
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
+        int ten = 0;
 
-        System.out.println();
+        // 10진수로 변환
+        for (int i = 0; i < m; i++) {
+            int square = m - i - 1;
+
+            ten += (int) (Math.pow(a, square) * (Integer.parseInt(st.nextToken())));
+        }
+
+        // b진수로 변환
+        while (ten > 0) {
+            stack.push(ten % b);
+            ten /= b;
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop()).append(" ");
+        }
+
+        System.out.println(sb);
+
         br.close();
     }
-
 }
