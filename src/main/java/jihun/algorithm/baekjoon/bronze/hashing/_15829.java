a54c8393de963669e2380aa812b80dfb3749fec4
jungjihun <jungjihun@unfine.co.kr>
2024-04-17 21:08:34 +0900

    해싱 추가

--- src/main/java/jihun/algorithm/baekjoon/bronze/hashing/_15829.java
+++ src/main/java/jihun/algorithm/baekjoon/bronze/hashing/_15829.java
@@ -8,22 +8,20 @@ public class _15829 {
 
     public static void main(String[] args) throws IOException {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final StringBuilder sb = new StringBuilder();
         final int NUMBER = 31;
-        final long M = 1234567891;
+        final int M = 1234567891;
 
-        int l = Integer.parseInt(br.readLine());
+        int L = Integer.parseInt(br.readLine());
         String str = br.readLine();
         long sum = 0L;
+        long pow = 1L;
 
-        // TODO 이거 pow 연산부터 오버플로우 발생하기 때문에 직접 수계산을 하는 방식으로 진행해야 할듯.
-        for (int i = 0; i < str.length(); i++) {
-            sum += (str.charAt(i) - 96) * ((long) Math.pow(NUMBER, i));
+        for (int i = 0; i < L; i++) {
+            sum += (str.charAt(i) - 96) * pow;
+            pow = (pow * NUMBER) % M;
         }
 
-        sb.append(sum % M);
-
-        System.out.println(sb);
+        System.out.println(sum % M);
         br.close();
     }
 
