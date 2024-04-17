2c19258697fab2efc82ced2c7bc9894e0e055e4d
jungjihun <jungjihun@unfine.co.kr>
2024-04-17 13:06:18 +0900

    해싱 작업중

--- src/main/java/jihun/algorithm/baekjoon/bronze/hashing/_15829.java
+++ src/main/java/jihun/algorithm/baekjoon/bronze/hashing/_15829.java
@@ -10,20 +10,18 @@ public class _15829 {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final StringBuilder sb = new StringBuilder();
         final int NUMBER = 31;
+        final long M = 1234567891;
 
         int l = Integer.parseInt(br.readLine());
         String str = br.readLine();
-
-        // TODO 빅인티저나 long형 초과 시 m으로 나누거나 하자 (오버플로우 체크)
         long sum = 0L;
 
+        // TODO 이거 pow 연산부터 오버플로우 발생하기 때문에 직접 수계산을 하는 방식으로 진행해야 할듯.
         for (int i = 0; i < str.length(); i++) {
-            long a = str.charAt(i) - 96;
-
-            sum += a * ((long) Math.pow(NUMBER, i));
+            sum += (str.charAt(i) - 96) * ((long) Math.pow(NUMBER, i));
         }
 
-        sb.append(sum);
+        sb.append(sum % M);
 
         System.out.println(sb);
         br.close();
