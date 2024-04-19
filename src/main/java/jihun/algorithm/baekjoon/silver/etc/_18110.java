88195eb70f60748677947938496527118e9a45f2
jungjihun <jungjihun@unfine.co.kr>
2024-04-19 16:38:58 +0900

    solved.ac - 구현 추가

---
+++ src/main/java/jihun/algorithm/baekjoon/silver/etc/_18110.java
@@ -0,0 +1,37 @@
+package jihun.algorithm.baekjoon.silver.etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * solved.ac - 구현
+ */
+public class _18110 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arrDifficulty = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arrDifficulty[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arrDifficulty);
+
+        int avgCount = Math.round(n * ((float) 15 / 100));
+        int sum = 0;
+
+        for (int i = avgCount; i < arrDifficulty.length - avgCount; i++) {
+            sum += arrDifficulty[i];
+        }
+
+        int result = Math.round((float) sum / (arrDifficulty.length - (avgCount * 2)));
+
+        System.out.println(result);
+        br.close();
+    }
+}
