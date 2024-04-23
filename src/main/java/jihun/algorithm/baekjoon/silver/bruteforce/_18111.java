556e49d00799ab9bc2859a8174d92e174f7295c2
jungjihun <jungjihun@unfine.co.kr>
2024-04-23 13:02:05 +0900

    마인크래프트 브루트포스 작업중

--- src/main/java/jihun/algorithm/baekjoon/silver/bruteforce/_18111.java
+++ src/main/java/jihun/algorithm/baekjoon/silver/bruteforce/_18111.java
@@ -6,111 +6,100 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * TODO 마인크래프트- 브루트 포스
- * 카운팅배열을 통해 가장 많이 나온 값을 찾아서 진행했었는데
- * 입력된 값 1개씩 비교하면서 하는게 맞는 것 같기도 하고... 내일 다시 생각해보자
+ * 마인크래프트- 브루트 포스
  */
 public class _18111 {
 
-    /**
-     * 가장 많이 나온 높이 값을 반환한다.
-     * @param arrCounting 카운팅 배열
-     * @return 최고 높이값
-     */
-    private static int getMaxCountHeight(int[] arrCounting) {
-        int maxCountHeight = -1;
-        int idx = -1;
-
-        for (int i = 0; i < arrCounting.length; i++) {
-            if (arrCounting[i] == 0) continue;
-
-            int tempCount = 0;
-            while (arrCounting[i]-- > 0) {
-                tempCount++;
-            }
-
-            if (tempCount > maxCountHeight) {
-                maxCountHeight = tempCount;
-                idx = i;
-            }
-        }
-
-        if (idx == -1) {
-            throw new IllegalStateException("index error");
-        }
-
-        return idx;
-    }
+//    /**
+//     * 가장 많이 나온 높이 값을 반환한다.
+//     * @param arrCounting 카운팅 배열
+//     * @return 최고 높이값
+//     */
+//    private static int getMaxCountHeight(int[] arrCounting) {
+//        int maxCountHeight = -1;
+//        int idx = -1;
+//
+//        for (int i = 0; i < arrCounting.length; i++) {
+//            if (arrCounting[i] == 0) continue;
+//
+//            int tempCount = 0;
+//            while (arrCounting[i]-- > 0) {
+//                tempCount++;
+//            }
+//
+//            if (tempCount > maxCountHeight) {
+//                maxCountHeight = tempCount;
+//                idx = i;
+//            }
+//        }
+//
+//        if (idx == -1) {
+//            throw new IllegalStateException("index error");
+//        }
+//
+//        return idx;
+//    }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        final int MAX_HEIGHT = 257;
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int[][] arrCraft = new int[n][m];
-
-        // 땅의 높이가 제일 많이 나온 수를 체크하는 카운팅 배열
-        int[] arrCounting = new int[257];
+        String answer = null;
 
         // 입력
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < m; j++) {
-                int tempHeight = Integer.parseInt(st.nextToken());
-                arrCraft[i][j] = tempHeight;
-                arrCounting[tempHeight]++;
+                arrCraft[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 가장 많이 나온 높이
-        int maxCountHeight = getMaxCountHeight(arrCounting);
+        // TODO 작업중
+        for (int i = 0; i < MAX_HEIGHT; i++) {
+            // 반복문 종료 조건
+            boolean isFinish = true;
+            int time = 0;
+            int invenCnt = b;
 
-        int time = 0;
-        int height = 0;
-        boolean isFinish = false;
-
-        while (!isFinish) {
             outer:
-            for(int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
+            for(int j = 0; j < n; j++) {
+                for (int k = 0; k < m; k++) {
+                    if (i == arrCraft[j][k]) continue;
 
-                    // 가장 많이 나온 높이보다 낮은 경우 같아질때까지 인벤토리에서 블록을 꺼내어 추가한다.
-                    if (arrCraft[i][j] < maxCountHeight) {
-                        int tempHeight = arrCraft[i][j];
-                        while (tempHeight != maxCountHeight) {
-                            // 인벤토리에 블록이 있으면 시간 +1
-                            if (b > 0) {
-                                b--;
-                                time++;
-                                tempHeight++;
-                            }
-                            // 인벤토리에 블록이 없으면(부족하면) 반복문 다시 돌기
-                            else {
+                    // 블록으로 땅을 채워야 하는 경우 (1초)
+                    if (i > arrCraft[j][k]) {
+                        while (i != arrCraft[j][k]) {
+                            if(invenCnt == 0) {
+                                isFinish = false;
                                 break outer;
                             }
+
+                            time++;
+                            invenCnt--;
                         }
                     }
-                    // 가장 많이 나온 높이보다 큰 경우 높이가 같아질때까지 블록 회수
-                    else if (arrCraft[i][j] > maxCountHeight) {
-                        int tempHeight = arrCraft[i][j];
-                        while (tempHeight != maxCountHeight) {
-                            tempHeight--;
-                            b++;
-                            time += 2;
-                        }
+                    // 땅 블록을 회수해야 하는 경우 (2초)
+                    else {
+                        time += 2;
+                        invenCnt++;
                     }
                 }
             }
 
-            isFinish = true;
+            // 가장 바깥 반복문 종료 조건
+            if (isFinish) {
+                answer = time + " " + i;
+                break;
+            }
         }
 
-        height = maxCountHeight;
-
-        System.out.println(time + " " + height);
+        System.out.println(answer);
         br.close();
     }
 }
