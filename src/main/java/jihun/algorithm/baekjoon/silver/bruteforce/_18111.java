566047a269457750fd69e8aa6d65af7082e2cabb
jungjihun <jungjihun@unfine.co.kr>
2024-04-26 15:21:52 +0900

    마인크래프트 1%에서 실패

--- src/main/java/jihun/algorithm/baekjoon/silver/bruteforce/_18111.java
+++ src/main/java/jihun/algorithm/baekjoon/silver/bruteforce/_18111.java
@@ -14,7 +14,7 @@ public class _18111 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        final int MAX_HEIGHT = 257;
+        final int MAX_INVEN_SIZE = 64000000;
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
@@ -22,18 +22,25 @@ public class _18111 {
         int maxHeight = -1;
         int minTime = 64000001 * 2;
 
+        // 가장 작은 값부터 큰 값까지 반복문을 돌려주기 위한 변수
+        int max = -1;
+        int min = 257;
+
         // 입력
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < m; j++) {
-                arrCraft[i][j] = Integer.parseInt(st.nextToken());
+                int input = Integer.parseInt(st.nextToken());
+                arrCraft[i][j] = input;
+
+                max = Math.max(input, max);
+                min = Math.min(input, min);
             }
         }
 
-        for (int i = 0; i < MAX_HEIGHT; i++) {
-            // 반복문 종료 조건
-            boolean isFinish = true;
+        for (int i = min; i <= max; i++) {
+            boolean isFail = false;
             int time = 0;
             int invenCnt = b;
 
@@ -44,34 +51,47 @@ public class _18111 {
 
                     if (i == tempHeight) continue;
 
-                    // 블록으로 땅을 채워야 하는 경우 (1초)
+                    // 인벤토리에서 블록을 꺼내어 땅을 채워야 하는 경우 (1초)
                     if (i > tempHeight) {
-                        while (i != tempHeight) {
-                            if(invenCnt == 0) {
-                                isFinish = false;
-                                break outer;
-                            }
+                        int subtraction = i - tempHeight;
 
-                            tempHeight++;
-                            time++;
-                            invenCnt--;
+                        // 인벤토리 블록 개수가 부족한 경우
+                        if (invenCnt < subtraction) {
+                            isFail = true;
+                            break outer;
                         }
+
+                        tempHeight += subtraction;
+                        time += subtraction;
+                        invenCnt -= subtraction;
                     }
+
                     // 땅 블록을 회수해야 하는 경우 (2초)
-                    else {
-                        while (i != tempHeight) {
-                            time += 2;
-                            invenCnt++;
-                            tempHeight--;
+                    if (i < tempHeight) {
+                        int addition = tempHeight - i;
+
+                        // 인벤토리 크기보다 회수한 블록의 개수가 많은 경우
+                        if (MAX_INVEN_SIZE < addition) {
+                            isFail = true;
+                            break outer;
                         }
+
+                        tempHeight -= addition;
+                        time += addition * 2;
+                        invenCnt += addition;
                     }
                 }
             }
 
-            // 가장 바깥 반복문 종료 조건
-            if (isFinish && (minTime > time)) {
+            // 시간이 동일하게 걸리면 제일 높이가 높은 것을 출력
+            if (!isFail && minTime == time) {
                 maxHeight = Math.max(i, maxHeight);
+            }
+
+            // 시간이 제일 적게 걸리는 경우 시간과 높이를 세팅
+            if (!isFail && minTime > time) {
                 minTime = time;
+                maxHeight = i;
             }
         }
 
