f63aaff15c7ececbacbd0fdb1fdbf7bd907c0f5f
jeongjihun <jihun3654@naver.com>
2024-09-08 19:37:02 +0900

    feat: A + B - C

---
+++ src/main/java/jihun/algorithm/baekjoon/bronze/math/_31403.java
@@ -0,0 +1,27 @@
+package jihun.algorithm.baekjoon.bronze.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * A + B - C
+ */
+public class _31403 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String a = br.readLine();
+        String b = br.readLine();
+        String c = br.readLine();
+
+        sb.append(Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c)).append("\n");
+        sb.append(Integer.parseInt(a + b) - Integer.parseInt(c));
+
+        System.out.println(sb);
+        br.close();
+    }
+
+}
