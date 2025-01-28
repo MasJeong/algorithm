70dbae907af83c2956bc77ab6feb68df3a76003f
jeongjihun <jihun3654@naver.com>
2025-01-28 16:02:32 +0900

    feat(HeapSort): 힙 정렬 추가함
    - 최대 힙으로 배열을 생성한 뒤 루트와 마지막 노드를 교환 후 다시 최대 힙으로 세팅하는 과정의 반복

--- src/main/java/jihun/algorithm/sort/heap/HeapSort.java
+++ src/main/java/jihun/algorithm/sort/heap/HeapSort.java
@@ -1,9 +1,8 @@
 package jihun.algorithm.sort.heap;
 
-import java.util.PriorityQueue;
-
 /**
- * 선택 정렬
+ * 힙 정렬 (오름차순)
+ * 1. 계수 정렬 (Counting Sort)
  * 2. 선택 정렬 (Selection Sort)
  * 3. 삽입 정렬 (Insertion Sort)
  * 4. 거품 정렬 (Bubble Sort)
@@ -11,17 +10,30 @@ import java.util.PriorityQueue;
  * 6. 힙 정렬 (Heap Sort) - 최소힙 구현
  * 7. 합병(병합) 정렬 (Merge Sort)
  * 8. 퀵 정렬 (Quick Sort)
+ * [구현 순서]
+ * 1. 최대 힙으로 배열을 생성
+ * 2. 루트 노드를 맨 뒤에 노드와 교환
+ * 3. 방금 교환한 맨 뒤에 노드를 제외하고 최대 힙으로 다시 세팅
+ * 4. 2~3 과정 반복
  *
  */
 public class HeapSort {
-	// TODO 작업예정
-	
-	// 부모 인덱스를 얻는 함수
+
+	/**
+	 * 부모 인덱스를 얻는 함수
+	 * @param child 자식 노드 인덱스
+	 * @return 부모 노드 인덱스
+	 */
 	private static int getParent(int child) {
 		return (child - 1) / 2;
 	}
 
-	// 두 인덱스의 원소를 교환하는 함수
+	/**
+	 * 두 인덱스의 원소를 교환하는 함수
+	 * @param a 배열
+	 * @param i 가장 큰 값 인덱스
+	 * @param j 부모 인덱스
+	 */
 	private static void swap(int[] a, int i, int j) {
 		int temp = a[i];
 		a[i] = a[j];
@@ -29,7 +41,12 @@ public class HeapSort {
 	}
 
 
-	// 힙을 만드는 함수
+	/**
+	 * 힙 생성
+	 * @param a 배열
+	 * @param parentIdx 부모 노드 인덱스
+	 * @param lastIdx 배열의 마지막 인덱스
+	 */
 	private static void heapify(int[] a, int parentIdx, int lastIdx) {
 
 		/*
@@ -77,6 +94,45 @@ public class HeapSort {
 	}
 
 	/**
+	 * 힙 정렬
+	 * @param a 입력 배열
+	 */
+	public static void heapsort(int[] a) {
+		int size = a.length;
+
+		/*
+		 * 부모노드와 heapify 과정에서 음수가 발생하면 잘못 된 참조가 발생하기 때문에
+		 * 원소가 1개이거나 0개일 경우는 정렬 할 필요가 없으므로 바로 함수를 종료한다.
+		 */
+		if(size < 2) {
+			throw new IllegalArgumentException("Heap sort requires at least 2 elements");
+		}
+
+		// 가장 마지막 노드의 부모 노드 인덱스
+		int parentIdx = getParent(size - 1);
+
+		// max heap 만들기
+		for(int i = parentIdx; i >= 0; i--) {
+			int lastIdx = size - 1;
+			// 부모노드(i값)을 1씩 줄이면서 heap 조건을 만족시키도록 재구성한다.
+			heapify(a, i, lastIdx);
+		}
+
+		// 정렬 과정
+		for(int i = size - 1; i > 0; i--) {
+
+			/*
+			 *  root인 0번째 인덱스와 i번째 인덱스의 값을 교환해준 뒤
+			 *  0 ~ (i-1) 까지의 부분트리에 대해 max heap을 만족하도록
+			 *  재구성한다.
+			 */
+			swap(a, 0, i);
+			heapify(a, 0, i - 1);
+		}
+
+	}
+
+	/**
 	 * heap: 최솟값 또는 최댓값을 빠르게 찾아내기 위해 완전이진트리 형태로 만들어진 자료구조.
 	 * heap 정렬을 알기 전 이진트리 개념이 필요하다.
 	 * - 이진 트리: 모든 노드의 최대 차수를 2로 제한한 것
@@ -93,26 +149,17 @@ public class HeapSort {
 	 * 1. 가장 작은 서브트리부터 최대 힙을 만족하도록 진행한다.
 	 */
 	public static void main(String[] args) {
-		int[] arr = {3, 7, 5, 4, 2, 8};
+		int[] a = {3, 7, 5, 4, 2, 8};
+
 		System.out.print(" 정렬 전 original 배열 : ");
-		for(int val : arr) {
+		for(int val : a) {
 			System.out.print(val + " ");
 		}
 
-		PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
-
-		// 배열을 힙으로 만든다.
-		for(int i = 0; i < arr.length; i++) {
-			heap.add(arr[i]);
-		}
-
-		// 힙에서 우선순위가 가장 높은 원소(root노드)를 하나씩 뽑는다.
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = heap.poll();
-		}
+		heapsort(a);
 
 		System.out.print("\n 정렬 후 배열 : ");
-		for(int val : arr) {
+		for(int val : a) {
 			System.out.print(val + " ");
 		}
 	}
